3ef29b50a86d80a7e865122b93c16c049bbdce5a
aimerzarashi <git.aimerzarashi.com>
2024-02-09 22:17:12 +0900

    uped test coverage

--- internal/domain/stock/location/aggregate_test.go
+++ internal/domain/stock/location/aggregate_test.go
@@ -16,13 +16,44 @@ func TestNewAggregate(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	name, err := location.NewName("test")
+	name, err := location.NewName("TestName")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// When
 	a := location.NewAggregate(id, name)
+
+	// Then
+	if a.Id != id {
+		t.Errorf("%T %+v want %+v", a.Id, a.Id, id)
+	}
+
+	if a.Name != name {
+		t.Errorf("%T %+v want %+v", a.Name, a.Name, name)
+	}
+
+	if a.IsDeleted() != false {
+		t.Errorf("%T %+v want %+v", a.IsDeleted(), a.IsDeleted(), false)
+	}
+}
+
+func TestRestoreAggregate(t *testing.T) {
+	t.Parallel()
+
+	// Given
+	id, err := location.NewId(uuid.New())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name, err := location.NewName("TestName")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// When
+	a := location.RestoreAggregate(id, name, false)
 
 	// Then
 	if a.Id != id {
--- internal/infra/repository/sqlboiler/stock/location/repository_test.go
+++ internal/infra/repository/sqlboiler/stock/location/repository_test.go
@@ -12,9 +12,10 @@ import (
 	"openapi/internal/infra/sqlboiler"
 
 	"github.com/google/uuid"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
-func TestCreate(t *testing.T) {
+func TestNewRepository(t *testing.T) {
 	t.Parallel()
 
 	// Setup
@@ -24,7 +25,45 @@ func TestCreate(t *testing.T) {
 	}
 	defer db.Close()
 
-	r, err := infra.NewRepository(db)
+	// When
+	repo, err := infra.NewRepository(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Then
+	if repo == nil {
+		t.Errorf("expected not nil, got nil")
+	}
+}
+
+func TestNewRepositoryFail(t *testing.T) {
+	t.Parallel()
+
+	// When
+	repo, err := infra.NewRepository(nil)
+
+	// Then
+	if err == nil {
+		t.Fatal("expected error but returned nil")
+	}
+
+	if repo != nil {
+		t.Errorf("expected nil, got not nil")
+	}
+}
+
+func TestSave(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	db, err := database.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	repo, err := infra.NewRepository(db)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -37,7 +76,7 @@ func TestCreate(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	name, err := domain.NewName("test")
+	name, err := domain.NewName("TestName")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -45,22 +84,22 @@ func TestCreate(t *testing.T) {
 	a := domain.NewAggregate(id, name)
 
 	// When
-	before, err := r.Get(a.Id)
+	before, err := repo.Get(a.Id)
 	if err == nil {
 		t.Fatalf("expected error but returned nil, %+v", before)
 	}
 
-	if err = r.Save(a); err != nil {
+	if err = repo.Save(a); err != nil {
 		t.Fatal(err)
 	}
 
-	after, err := r.Get(a.Id)
+	after, err := repo.Get(a.Id)
 	if err != nil {
 		t.Fatalf("expected error but returned nil, %+v", err)
 	}
 
 	// Then
-	if reflect.DeepEqual(after, before) {
+	if reflect.DeepEqual(after,before) {
 		t.Errorf("%T %+v want %+v", after, after, before)
 	}
 
@@ -100,7 +139,7 @@ func TestUpdate(t *testing.T) {
 	}
 	defer db.Close()
 
-	r, err := infra.NewRepository(db)
+	repo, err := infra.NewRepository(db)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -121,7 +160,7 @@ func TestUpdate(t *testing.T) {
 	currentDateTime := time.Now().UTC()
 	dataFormat := "2006-01-02 15:04:05.000000 +09:00"
 
-	if err = r.Save(before); err != nil {
+	if err = repo.Save(before); err != nil {
 		t.Fatal(err)
 	}
 
@@ -131,7 +170,7 @@ func TestUpdate(t *testing.T) {
 	}
 
 	// When
-	after, err := r.Get(before.Id)
+	after, err := repo.Get(before.Id)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -144,7 +183,7 @@ func TestUpdate(t *testing.T) {
 	after.Name = changedName
 	after.Delete()
 
-	if err = r.Save(after); err != nil {
+	if err = repo.Save(after); err != nil {
 		t.Fatal(err)
 	}
 
@@ -185,7 +224,7 @@ func TestFind(t *testing.T) {
 	}
 	defer db.Close()
 
-	r, err := infra.NewRepository(db)
+	repo, err := infra.NewRepository(db)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -204,16 +243,16 @@ func TestFind(t *testing.T) {
 	a := domain.NewAggregate(id, name)
 
 	// When
-	notFound, err := r.Find(a.Id)
+	notFound, err := repo.Find(a.Id)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err = r.Save(a); err != nil {
+	if err = repo.Save(a); err != nil {
 		t.Fatal(err)
 	}
 
-	found, err := r.Find(a.Id)
+	found, err := repo.Find(a.Id)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -225,5 +264,109 @@ func TestFind(t *testing.T) {
 
 	if found != true {
 		t.Errorf("%T %+v want %+v", found, found, true)
+	}
+}
+
+func TestGetFailInvalidData(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	db, err := database.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	repo, err := infra.NewRepository(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Given
+	id, err := domain.NewId(uuid.New())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name, err := domain.NewName("TestName")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a := domain.NewAggregate(id, name)
+
+	if err := repo.Save(a); err != nil {
+		t.Fatal(err)
+	}
+
+	data := &sqlboiler.StockLocation{
+		ID:   a.Id.String(),
+		Name: "",
+		Deleted: a.IsDeleted(),
+	}
+
+	err = data.Upsert(
+		context.Background(),
+		db,
+		true,
+		[]string{"id"},
+		boil.Whitelist("name","deleted"),
+		boil.Infer(),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// When
+	_, err = repo.Get(id)
+
+	// Then
+	if err == nil {
+		t.Fatal("expected error but returned nil")
+	}
+}
+
+func TestFailDbClose(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	db, err := database.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.Close()
+
+	repo, err := infra.NewRepository(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Given
+	id, err := domain.NewId(uuid.New())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name, err := domain.NewName("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a := domain.NewAggregate(id, name)
+
+	// Then
+	_, err = repo.Get(a.Id)
+	if err == nil {
+		t.Fatal("expected error but returned nil")
+	}
+
+	_, err = repo.Find(a.Id)
+	if err == nil {
+		t.Fatal("expected error but returned nil")
+	}
+
+	err = repo.Save(a)
+	if err == nil {
+		t.Fatal("expected error but returned nil")
 	}
 }
