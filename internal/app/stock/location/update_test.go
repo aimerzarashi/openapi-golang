95152ef24b8f9758fdbf0da14021ead1f84a0afc
aimerzarashi <git.aimerzarashi.com>
2024-02-10 20:51:31 +0900

    added test case

--- internal/app/stock/location/update_test.go
+++ internal/app/stock/location/update_test.go
@@ -1,12 +1,15 @@
 package location_test
 
 import (
+	"errors"
 	app "openapi/internal/app/stock/location"
+	mock "openapi/internal/app/stock/location/internal"
 	domain "openapi/internal/domain/stock/location"
 	"openapi/internal/infra/database"
 	infra "openapi/internal/infra/repository/sqlboiler/stock/location"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 )
 
@@ -80,5 +83,72 @@ func TestUpdateFailIdNil(t *testing.T) {
 	_, err := app.NewUpdateRequest(uuid.Nil, name)
 	if err != domain.ErrInvalidId {
 		t.Errorf("%T %v, want %v", err, err, domain.ErrInvalidId)
+	}
+}
+
+func TestUpdateFail(t *testing.T) {
+	t.Parallel()
+	
+	// Setup
+	db, err := database.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	repo, err := infra.NewRepository(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// When
+	newName := "test"
+	reqUpdate, err := app.NewUpdateRequest(uuid.New(), newName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = app.Update(reqUpdate, repo)
+
+	// Then
+	if err == nil {
+		t.Errorf("expected not nil, got nil")
+	}
+}
+
+func TestUpdateFailSaveFail(t *testing.T) {
+	t.Parallel()
+
+	// Setup
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	id, err := domain.NewId(uuid.New())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name, err := domain.NewName("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a := domain.NewAggregate(id, name)
+
+	repo := mock.NewMockIRepository(ctrl)
+	repo.EXPECT().Get(gomock.Any()).Return(a, nil)
+	repo.EXPECT().Save(gomock.Any()).Return(errors.New("test error"))
+
+	// When
+	reqUpdate, err := app.NewUpdateRequest(id.UUID(), name.String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = app.Update(reqUpdate, repo)
+
+	// Then
+	if err == nil {
+		t.Errorf("expected not nil, got nil")
 	}
 }
