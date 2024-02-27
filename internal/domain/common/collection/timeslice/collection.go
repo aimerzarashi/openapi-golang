da5721ead2e8fb9a5ad197e792ec36b4a629d02b
aimerzarashi <git.aimerzarashi.com>
2024-02-27 21:44:50 +0900

    added add method

--- internal/domain/common/collection/timeslice/collection.go
+++ internal/domain/common/collection/timeslice/collection.go
@@ -56,3 +56,29 @@ func (d Collection[T]) Find(criteria time.Time) (Item[T], error) {
 
 	return Item[T]{}, ErrCollectionNotFound
 }
+
+func (d *Collection[T]) Add(adding *Item[T]) (*Collection[T], error) {
+	buffer := make([]*Item[T], len(d.items))
+	buffer = append(buffer, adding)
+
+	// 追加する期間が重複している場合は、追加する期間を優先して既存の期間を調整する
+	for _, v := range d.items {
+		adjusting, err := v.Adjust(adding)
+		if err != nil {
+			return nil, err
+		}
+		buffer = append(buffer, adjusting...)
+	}
+
+	// startAtで昇順ソートする
+	sort.Slice(buffer, func(i, j int) bool {
+		return buffer[i].StartAt().Before(buffer[j].StartAt())
+	})
+
+	// 調整済みの期間に置き換える
+	items, err := NewCollection(buffer...)
+	if err != nil {
+		return nil, err
+	}
+	return items, nil
+}
--- internal/domain/common/collection/timeslice/collection_test.go
+++ internal/domain/common/collection/timeslice/collection_test.go
@@ -20,9 +20,12 @@ func TestNewCollection(t *testing.T) {
 	// Setup
 	t.Parallel()
 
-	a := "a"
-
 	type T = string
+
+	value1 := "value1"
+	value2 := "value2"
+	value3 := "value3"
+
 	type args struct {
 		items []*timeslice.Item[T]
 	}
@@ -49,9 +52,9 @@ func TestNewCollection(t *testing.T) {
 			name: "success/not empty",
 			args: args{
 				items: []*timeslice.Item[T]{
-					NewItem(&a, time.Date(2024, 1, 1, 9, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC)),
-					NewItem(&a, time.Date(2024, 1, 1, 10, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 10, 59, 59, 0, time.UTC)),
-					NewItem(&a, time.Date(2024, 1, 1, 11, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 11, 59, 59, 0, time.UTC)),
+					NewItem(&value1, time.Date(2024, 1, 1, 9, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC)),
+					NewItem(&value2, time.Date(2024, 1, 1, 10, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 10, 59, 59, 0, time.UTC)),
+					NewItem(&value3, time.Date(2024, 1, 1, 11, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 11, 59, 59, 0, time.UTC)),
 				},
 			},
 			want: want{
@@ -63,9 +66,9 @@ func TestNewCollection(t *testing.T) {
 			name: "fail",
 			args: args{
 				items: []*timeslice.Item[T]{
-					NewItem(&a, time.Date(2024, 1, 1, 9, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC)),
-					NewItem(&a, time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC), time.Date(2024, 1, 1, 10, 59, 59, 0, time.UTC)),
-					NewItem(&a, time.Date(2024, 1, 1, 11, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 11, 59, 59, 0, time.UTC)),
+					NewItem(&value1, time.Date(2024, 1, 1, 9, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC)),
+					NewItem(&value2, time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC), time.Date(2024, 1, 1, 10, 59, 59, 0, time.UTC)),
+					NewItem(&value3, time.Date(2024, 1, 1, 11, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 11, 59, 59, 0, time.UTC)),
 				},
 			},
 			want: want{
@@ -91,6 +94,156 @@ func TestNewCollection(t *testing.T) {
 					if item != tt.args.items[i] {
 						t.Errorf("NewCollection() = %+v, want %+v", item, tt.args.items[i])
 					}
+				}
+				return
+			}
+
+			if !errors.Is(err, tt.want.err) {
+				t.Errorf("NewCollection() error = %v, wantErr %v", err, tt.want.err)
+			}
+		})
+	}
+}
+
+func TestCollection_Find(t *testing.T) {
+	// Setup
+	t.Parallel()
+
+	type T = string
+
+	value1 := "value1"
+	value2 := "value2"
+	value3 := "value3"
+
+	items := []*timeslice.Item[T]{
+		NewItem(&value1, time.Date(2024, 1, 1, 9, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC)),
+		NewItem(&value2, time.Date(2024, 1, 1, 10, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 10, 59, 59, 0, time.UTC)),
+		NewItem(&value3, time.Date(2024, 1, 1, 11, 0, 0, 0, time.UTC), time.Date(2024, 1, 1, 11, 59, 59, 0, time.UTC)),
+	}
+
+	type args struct {
+		criteria time.Time
+	}
+	type want struct {
+		item *timeslice.Item[T]
+		err error
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+		wantErr bool
+	}{
+		{
+			name: "success/1",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 9, 0, 0, 0, time.UTC),
+			},
+			want: want{
+				item: items[0],
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "success/2",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 9, 59, 59, 0, time.UTC),
+			},
+			want: want{
+				item: items[0],
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "success/3",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 10, 0, 0, 0, time.UTC),
+			},
+			want: want{
+				item: items[1],
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "success/4",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 10, 59, 59, 0, time.UTC),
+			},
+			want: want{
+				item: items[1],
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "success/5",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 11, 0, 0, 0, time.UTC),
+			},
+			want: want{
+				item: items[2],
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "success/6",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 11, 59, 59, 0, time.UTC),
+			},
+			want: want{
+				item: items[2],
+				err: nil,
+			},
+			wantErr: false,
+		},
+		{
+			name: "fail/1",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 8, 59, 59, 0, time.UTC),
+			},
+			want: want{
+				item: nil,
+				err: timeslice.ErrCollectionNotFound,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail/2",
+			args: args{
+				criteria: time.Date(2024, 1, 1, 12, 0, 0, 0, time.UTC),
+			},
+			want: want{
+				item: nil,
+				err: timeslice.ErrCollectionNotFound,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Given
+			timeSlice, err := timeslice.NewCollection(items...)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// When
+			got, err := timeSlice.Find(tt.args.criteria)
+
+			// Then
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("NewCollection() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				if got.Value() != tt.want.item.Value() {
+					t.Errorf("NewCollection() = %v, want %v", got, tt.want.item)
 				}
 				return
 			}
